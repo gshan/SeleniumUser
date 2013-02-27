db55d1689f1be9aafe14109b35450abbfee07027
danzajork <danzajork@gmail.com>
2013-02-26 21:11:37 -0600

    refactor
    Removing the searchFrames craziness, adding FrameContext

---
+++ SeleniumUser/FrameContext.cs
@@ -0,0 +1,40 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumUser
+{
+    public class FrameContext : IDisposable
+    {
+        private IWebDriver Driver { get; set; }
+
+        /// <summary>
+        /// Switches the web driver context to the specified iFrame.
+        /// If the iFrame is contained within another iFrame, specifiy the path to the iFrame.
+        /// </summary>
+        /// <param name="driver">Web driver</param>
+        /// <param name="frameNames">Frame names or IDs</param>
+        public FrameContext(IWebDriver driver, params string[] frameNames)
+        {
+            this.Driver = driver;
+
+            NavigateToFrame(frameNames);
+        }
+
+        private void NavigateToFrame(params string[] frameNames)
+        {
+            foreach (var frameName in frameNames)
+            {
+                this.Driver.SwitchTo().Frame(frameName);
+            }
+        }
+
+        public void Dispose()
+        {
+            this.Driver.SwitchTo().DefaultContent();
+        }
+    }
+}
--- SeleniumUser/User.cs
+++ SeleniumUser/User.cs
@@ -44,15 +44,17 @@ namespace SeleniumUser
                 Thread.Sleep(RETRY_DELAY);
             }
 
-            (Driver as ITakesScreenshot).GetScreenshot().SaveAsFile(Path.Combine(ScreenshotDirectory, "failure.png"), ImageFormat.Png);
-
-            Assert.Fail(message);
+            FailTest(message);
         }
 
         public static void Wait(string message, Action action, int tryCount = 1)
         {
             if (tryCount >= MAX_RETRIES)
+            {
+                FailTest(message);
+
                 return;
+            }
            
             try
             {
@@ -61,60 +63,37 @@ namespace SeleniumUser
             catch (Exception e)
             {
                 Debug.WriteLine("Wait caught exception {0} for '{1}', retrying...", e.Message, message);
-                
-                Thread.Sleep(RETRY_DELAY);
-
-                tryCount += 1;
-
-                Wait(message, action, tryCount);
             }
-            
+
+            Thread.Sleep(RETRY_DELAY);
+
+            tryCount += 1;
+
+            Wait(message, action, tryCount);
         }
 
-        public static void ShouldSee(By by, bool searchFrames = false)
+        private static void FailTest(string message)
+        {
+            (Driver as ITakesScreenshot).GetScreenshot().SaveAsFile(Path.Combine(ScreenshotDirectory, "failure.png"), ImageFormat.Png);
+
+            Assert.Fail(message);
+        }
+
+        public static void ShouldSee(By by)
         {
             Debug.WriteLine(by + " should be visible");
 
-            if (PageHasIFrames() && searchFrames)
-            {
-                if (!SearchForElementInFrames(by, FrameNode.FindFrames(Driver)))
-                {
-                    Assert.Fail("Could not find element by" + @by);
-                }
-            }
-            else
-            {
-                Wait(@by + " should be visible", () =>
-                    Driver.FindElement(by).Displayed ||
-                    Driver.FindElement(by).GetAttribute("visability") == "visible");
-            }
+            Wait(@by + " should be visible", () =>
+                Driver.FindElements(by).Any(x => x.Displayed) ||
+                Driver.FindElements(by).Any(x => x.GetAttribute("visability") == "visible"));
         }
 
-        private static bool SearchForElementInFrames(By by, FrameNode node)
+        public static void ShouldSeeText(By by, string text)
         {
-            foreach (var n in node.Frames)
-            {
-                FrameNode.NavigateToNode(Driver, n);
+            Debug.WriteLine(@by + " should contain " + text);
 
-                if (Driver.FindElements(by).Any(x => x.Displayed) ||
-                    Driver.FindElement(by).GetAttribute("visability") == "visible")
-                {
-                    Driver.SwitchTo().DefaultContent();
-                    return true;
-                }
-
-                if (n.HasFrames)
-                {
-                    if (SearchForElementInFrames(by, n))
-                    {
-                        return true;
-                    }
-
-                    FrameNode.NavigateToNode(Driver, n);
-                }
-            }
-
-            return false;
+            Wait(@by + " should contain " + text, () => 
+                Driver.FindElements(by).Any(x => x.Text.Contains(text) && x.Displayed));
         }
 
         public static void ShouldNotSee(By by)
@@ -136,147 +115,40 @@ namespace SeleniumUser
             });
         }
 
-        public static void Clicks(By by, bool searchFrames = false)
+        public static void Clicks(By by)
         {
             Debug.WriteLine("Clicking " + by);
 
-            if (PageHasIFrames() && searchFrames)
-            {
-                var wasPerformed =
-                    PerformActionInFrame(() =>
-                        {
-                            var element = Driver.FindElements(by).SingleOrDefault();
-                            return element != null;
-                        }, () =>
-                        {
-                            Wait(@by + " attempting click", () =>
-                                {
-                                    Driver.FindElement(by).Click();
-                                    return true;
-                                });
-
-                            Driver.SwitchTo().DefaultContent();
-                        }, FrameNode.FindFrames(Driver));
-
-                if (!wasPerformed)
+            Wait(@by + " attempting click", () =>
                 {
-                    Assert.Fail(@by + " was not clicked");
-                }
-            }
-            else
-            {
-                Wait(@by + " attempting click", () =>
-                    {
-                        Driver.FindElement(by).Click();
-                        return true;
-                    });
-            }
-        }
-
-        public static void Clicks(By by, string text, bool searchFrames = false)
-        {
-            if (PageHasIFrames() && searchFrames)
-            {
-                var wasPerformed =
-                    PerformActionInFrame(() =>
-                        {
-                            var element = Driver.FindElements(by).SingleOrDefault(x => x.Text.Contains(text));
-
-                            return element != null;
-                        },
-                                         () =>
-                                             {
-                                                 var element =
-                                                     Driver.FindElements(by).SingleOrDefault(x => x.Text.Contains(text));
-                                                 Wait(@by + " attempting click", () => element.Click());
-                                                 Driver.SwitchTo().DefaultContent();
-                                             }, FrameNode.FindFrames(Driver));
-
-                if (!wasPerformed)
-                {
-                    Assert.Fail(@by + " was not clicked");
-                }
-            }
-            else
-            {
-                var element = Driver.FindElements(by).SingleOrDefault(x => x.Text.Contains(text));
-
-                Wait("Attempting click", () => element.Click());
-            }
-        }
-
-        public static bool ShouldSeeText(By by, string text, bool searchFrames = false)
-        {
-            if (PageHasIFrames() && searchFrames)
-            {
-                if (!SearchForTextInFrames(by, text, FrameNode.FindFrames(Driver)))
-                {
-                    Assert.Fail("Could not find text " + text);
-                }
-            }
-            else
-            {
-                Wait(@by + " should contain " + text, () => Driver.FindElements(by).Any(x => x.Text.Contains(text) && x.Displayed));
-            }
-
-            return false;
-        }
-
-        private static bool SearchForTextInFrames(By by, string text,  FrameNode node)
-        {
-            foreach (var n in node.Frames)
-            {
-                FrameNode.NavigateToNode(Driver, n);
-
-                if (Driver.FindElements(by).Any(x => x.Text.Contains(text) && x.Displayed))
-                {
-                    Driver.SwitchTo().DefaultContent();
+                    Driver.FindElement(by).Click();
                     return true;
-                }
-
-                if (n.HasFrames)
-                {
-                    if (SearchForTextInFrames(by, text, n))
-                    {
-                        return true;
-                    }
-
-                    FrameNode.NavigateToNode(Driver, n);
-                }
-            }
-
-            return false;
+                });
         }
 
-        public static void InputText(By by, string text, bool searchFrames = false)
+        public static void Clicks(By by, string text)
+        {
+            var element = Driver.FindElements(by).SingleOrDefault(x => x.Text.Contains(text));
+
+            Wait("Attempting click", () => element.Click());
+        }
+
+        public static void InputText(By by, string text)
         {
             Debug.WriteLine("Inputing " + by);
 
-            Action inputText = () =>
-            {
-                Wait(@by + " should be intputable", () =>
-                    {
-                        Driver.FindElement(by).SendKeys(text);
-                        return true;
-                    });
-            };
-
-            if (searchFrames)
-            {
-                PerformActionInFrame(() => HasElement(by), inputText, FrameNode.FindFrames(Driver));
-                return;
-            }
-
-            inputText();
+            Wait(@by + " should be intputable", () =>
+                {
+                    Driver.FindElement(by).SendKeys(text);
+                    return true;
+                });
         }
 
-        public static void MakeSelection(By by, string selectText, bool searchFrames = false)
+        public static void MakeSelection(By by, string selectText)
         {
             Debug.WriteLine("Making selection on " + by);
-
-            Action makeSelection = () =>
-            {
-                Wait(@by + " should be selectable", () =>
+     
+            Wait(@by + " should be selectable", () =>
                 {
                     var dropDownListBox = Driver.FindElement(by);
                     var element = new SelectElement(dropDownListBox);
@@ -285,75 +157,23 @@ namespace SeleniumUser
 
                     return true;
                 });
-            };
-
-            if (searchFrames)
-            {
-                PerformActionInFrame(() => HasElement(by), makeSelection, FrameNode.FindFrames(Driver));
-                return;
-            }
-
-            makeSelection();
         }
 
-        public static void ClearsInput(By by, bool searchFrames = false)
+        public static void ClearsInput(By by)
         {
             Debug.WriteLine("Clearing " + by);
 
-            Action clearInput = () =>
-            {
-                Wait(@by + " should be clearable", () =>
+            Wait(@by + " should be clearable", () =>
                 {
                     Driver.FindElement(by).Clear();
                     return true;
                 });
-            };
-
-            if (searchFrames)
-            {
-                PerformActionInFrame(() => HasElement(by), clearInput, FrameNode.FindFrames(Driver));
-                return;
-            }
-
-            clearInput();
-        }
-
-        private static bool PageHasIFrames()
-        { 
-            var frames = Driver.FindElements(By.TagName("iframe"));
-
-            return frames != null && frames.Any(); 
         }
 
         private static bool HasElement(By by)
         {
             var element = Driver.FindElements(by).SingleOrDefault();
             return element != null;
-        }
-
-        private static bool PerformActionInFrame(Func<bool> condition, Action action, FrameNode node)
-        {
-            foreach (var n in node.Frames)
-            {
-                FrameNode.NavigateToNode(Driver, n);
-
-                if (condition())
-                {
-                    action();
-                    return true;
-                }
-
-                if (!n.HasFrames) continue;
-
-                if (PerformActionInFrame(condition, action, n))
-                {
-                    return true;
-                }
-
-                FrameNode.NavigateToNode(Driver, n);
-            }
-
-            return false;
         }
     }
 }
